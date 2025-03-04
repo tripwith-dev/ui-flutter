48b96890c757f18450de566ebfc6c803d124fca8
Dohyeon Won <dh1072005@gmail.com>
2025-03-04 19:01:22 +0900

    init: android setting

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.1.12297006"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
